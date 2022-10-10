48dc0ab869ea0f1875bfe9e14b618fd1aaa7b891
Aditya-Soni23 <77341618+Aditya-Soni23@users.noreply.github.com>
2022-10-10 22:21:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function te(){
+    window.location = "conta.html";
+}
+function to(){
+    window.location = "pack.html";
+}
+function ba(){
+    window.location = "index.html";
+}
+function po(){
+    window.location = "reg.html";
+}
+function li(){
+    window.location = "qu.html";
+}
+function v(){
+    window.location = "col.html";
+}
+
